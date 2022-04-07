43fc9fc6cfa7561c608ba2c5ace3e69658ec00f6
jingwenyvonne <72360610+jingwenyvonne@users.noreply.github.com>
2022-04-07 14:43:44 -0500

    Update and rename config.ini to config.py

---
+++ config.py
@@ -0,0 +1,19 @@
+# credentials
+un = "jewelrymdjewelry"
+pw = "kaviglobal123_"
+
+# Specify which hashtags you want to explore
+hashtag_list = ["gold","accessories","earrings","necklace","silver","handmadejewelry",
+                "giftideas","bracelet","ring","etsyshop","diamond","diamonds","bracelets","couture","jewelrydesigner",
+               "jewelryaddict","fashionjewelry","jewelrydesign","jewelryaddict","fashionjewelry","instajewelry",
+               "jewelrygram","gems","gemstone","pendant","gemstones","jewels","jewelry","smallbusiness"]
+
+#Specify comments
+comments_list=["Glowing through <3","Simply Iconic, I'm in love!",
+               "This is amazing! I can't wait to wear it with @jewelrymdjewelry","An absolute example of perfect beauty<3",
+              "This goes so well with @jewelrymdjewelry!",
+               "Same to what @jewelrymdjewelry has, this picture has all the inspiration I needed in my life!",
+              "This is definitely what @jewelrymdjewelry portrays all the time - beauty in simplicity <3",
+              "That is a gorgeous layered look! Many can also be found at @jewelrymdjewelry",
+              "A big mood right here <3",
+               "Just put in an order but I think I need to order all of these to match @jewelrymdjewelry!"]
