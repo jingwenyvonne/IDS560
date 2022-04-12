5c252194a0946c8d562331de1612cde254ea86de
jingwenyvonne <72360610+jingwenyvonne@users.noreply.github.com>
2022-04-12 15:11:19 -0500

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from random import randint
 import pandas as pd
 from selenium.webdriver.common.by import By
 import time
+from config import *
 
 
 # In[34]:
@@ -19,29 +20,6 @@ chromedriver_path = "/usr/local/bin/chromedriver"
 webdriver = webdriver.Chrome(executable_path = chromedriver_path)
 webdriver.get("https://www.instagram.com/accounts/login")
 sleep(1)
-
-
-# In[35]:
-
-
-# Enter username/password to log in
-un, pw = "jewelrymdjewelry","kaviglobal123_"
-
-# Specify which hashtags you want to explore
-hashtag_list = ["gold","accessories","earrings","necklace","silver","handmadejewelry",
-                "giftideas","bracelet","ring","etsyshop","diamond","diamonds","bracelets","couture","jewelrydesigner",
-               "jewelryaddict","fashionjewelry","jewelrydesign","jewelryaddict","fashionjewelry","instajewelry",
-               "jewelrygram","gems","gemstone","pendant","gemstones","jewels","jewelry","smallbusiness"]
-
-#Specify comments
-comments_list=["Glowing through <3","Simply Iconic, I'm in love!",
-               "This is amazing! I can't wait to wear it with @jewelrymdjewelry","An absolute example of perfect beauty<3",
-              "This goes so well with @jewelrymdjewelry!",
-               "Same to what @jewelrymdjewelry has, this picture has all the inspiration I needed in my life!",
-              "This is definitely what @jewelrymdjewelry portrays all the time - beauty in simplicity <3",
-              "That is a gorgeous layered look! Many can also be found at @jewelrymdjewelry",
-              "A big mood right here <3",
-               "Just put in an order but I think I need to order all of these to match @jewelrymdjewelry!"]
 
 
 limits = {}
